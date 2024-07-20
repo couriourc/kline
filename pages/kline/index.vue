cd14dd0265044f5b92b0480dffbd3484a7d28fdd
godakid <godakid@outlook.com>
2024-07-20 21:19:52 +0800

    Splitter Layout

--- pages/kline/index.vue
+++ pages/kline/index.vue
@@ -1,14 +1,20 @@
 <template>
   <ClientOnly>
-    <VueSplitter class="bg-transparent" @splitter-click="resize" v-model:percent="limitedPercent"
-                 initial-percent="80">
+    <VueSplitter class="bg-transparent
+    ![&_.splitter]:h-full ![&_.splitter]:bg-blue ![&_.splitter]:w-4px
+"
+                 @splitter-click="resize"
+                 v-model:percent="limitedPercent"
+                 initial-percent="95"
+    >
       <template #left-pane>
         <lazy-kline :menu-options="actions" ref="chartRef"></lazy-kline>
       </template>
       <template #right-pane>
         <div class="h-full flex  w-full pr-12px active:shadow-lg duration-100ms">
-
-          <div class="h-full w-full flex flex-col gap-2px rounded-l-8px overflow-hidden hover:shadow-lg">
+          <div class="h-full w-full flex flex-col gap-2px rounded-l-8px overflow-hidden hover:shadow-lg"
+               v-if="!!rightViewKey"
+          >
             <div class="header px-12px flex items-center h-38px bg-white ">
               <u-dropdown :items="items" :ui="{ item: { disabled: 'cursor-text select-text' } }"
                           :popper="{ placement: 'bottom-start' }">
@@ -30,7 +36,28 @@
           </div>
 
           <div
-              class="w-48px flex-col flex  h-full items-center py-6px  active:shadow-lg duration-100ms bg-#FFF border-l-1 border-gray-100">
+              class="w-58px flex-col flex  h-full items-center py-6px  active:shadow-lg duration-100ms bg-#FFF border-l-1 border-gray-100">
+
+            <button
+                class="p-2px outline-none
+                border-0
+                border-transparent
+                border-l-1px
+                hover:bg-blue-50
+                w-full
+                rounded-2px
+                 border-solid
+                peer-[.active]:border-blue group flex items-center flex-center
+"
+                @click="rightViewKey = !rightViewKey? 'data':null"
+            >
+              <ui-icon :icon="!!rightViewKey?'nav-expand':'nav-collapse'"
+
+                       class="text-20px  group-hover:drop-shadow-sm  group-hover:text-blue">
+
+              </ui-icon>
+            </button>
+
 
             <button
                 class="p-2px outline-none
@@ -85,7 +112,7 @@ const limitedPercent = computed({
     return percent.value;
   },
   set(val: number) {
-    percent.value = Math.max(70, Math.min(90, val));
+    percent.value = Math.max(70, Math.min(95, val));
   }
 });
 const items = [
@@ -99,4 +126,12 @@ const items = [
   }],
 ];
 
+const rightViewKey = ref<string | null>(null);
+watch(rightViewKey, () => {
+  if (!rightViewKey.value) {
+    limitedPercent.value = 95;
+  } else {
+    limitedPercent.value = 80;
+  }
+});
 </script>
